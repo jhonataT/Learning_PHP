08755fa51378cf88b456cda006f521f634240def
Jhn <jhonata.tenorio2526@gmail.com>
2020-12-25 17:49:11 -0300

    strings01

---
+++ strings/exemplo01.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = "teste1"; // string;
+    $nome2 = 'teste2'; // string;
+
+    var_dump($nome, $nome2);
+
+    echo "ABC $nome"; // entre aspas duplas, é possível chamar o conteúdo da vaiável dentro.
+
+?>
