c47dcd7ad532805147ab5fb24c3343820e3b6357
Jhn <jhonata.tenorio2526@gmail.com>
2020-12-28 12:20:04 -0300

    functions Upper and Lower Case

---
+++ strings/exemplo02.php
@@ -0,0 +1,15 @@
+<?php
+// ToUpperCase():
+
+    $nome = 'Jhonata dos santos';
+    echo strtoupper($nome);
+
+    // ToLowerCase():
+    echo '<br/>' . strtolower($nome);
+
+    // Cade letra de cada palavra para UpperCase:
+    echo '<br/>' . ucwords($nome);
+
+    // Primeira letra para UpperCase:
+    echo '<br/>' . ucFirst($nome);
+?>
