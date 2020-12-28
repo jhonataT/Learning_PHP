6bd95780e211a89c650b3e9c31391e1bf8a02b05
Jhn <jhonata.tenorio2526@gmail.com>
2020-12-28 12:24:17 -0300

    replace()

---
+++ strings/exemplo03.php
@@ -0,0 +1,11 @@
+<?php
+// Substituir substrings de uma string: (replace);
+    $nome = 'Jhonata dos Santos';
+    
+    echo "<br/> $nome";
+
+    $nome = str_replace('dos Santos', 'Tenorio', $nome);
+
+    echo "<br/> $nome";
+
+?>
